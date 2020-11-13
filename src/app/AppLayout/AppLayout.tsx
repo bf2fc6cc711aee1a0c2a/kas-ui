6a19352e3b0d19fdd235d3331d5795376182a361
Christie Molloy <cmolloy@redhat.com>
2020-11-13 10:39:47 -0500

    add pf react seed app

---
+++ src/app/AppLayout/AppLayout.tsx
@@ -0,0 +1,106 @@
+import * as React from 'react';
+import { NavLink, useLocation, useHistory } from 'react-router-dom';
+import {
+  Nav,
+  NavList,
+  NavItem,
+  NavExpandable,
+  Page,
+  PageHeader,
+  PageSidebar,
+  SkipToContent
+} from '@patternfly/react-core';
+import { routes, IAppRoute, IAppRouteGroup } from '@app/routes';
+import logo from '@app/bgimages/Patternfly-Logo.svg';
+
+interface IAppLayout {
+  children: React.ReactNode;
+}
+
+const AppLayout: React.FunctionComponent<IAppLayout> = ({children}) => {
+  const [isNavOpen, setIsNavOpen] = React.useState(true);
+  const [isMobileView, setIsMobileView] = React.useState(true);
+  const [isNavOpenMobile, setIsNavOpenMobile] = React.useState(false);
+  const onNavToggleMobile = () => {
+    setIsNavOpenMobile(!isNavOpenMobile);
+  };
+  const onNavToggle = () => {
+    setIsNavOpen(!isNavOpen);
+  }
+  const onPageResize = (props: { mobileView: boolean; windowSize: number }) => {
+    setIsMobileView(props.mobileView);
+  };
+
+  function LogoImg() {
+    const history = useHistory();
+    function handleClick() {
+      history.push('/');
+    }
+    return (
+      <img src={logo} onClick={handleClick} alt="PatternFly Logo" />
+    );
+  }
+
+  const Header = (
+    <PageHeader
+      logo={<LogoImg />}
+      showNavToggle
+      isNavOpen={isNavOpen}
+      onNavToggle={isMobileView ? onNavToggleMobile : onNavToggle}
+    />
+  );
+
+  const location = useLocation();
+
+  const renderNavItem = (route: IAppRoute, index: number) => (
+    <NavItem key={`${route.label}-${index}`} id={`${route.label}-${index}`}>
+      <NavLink exact to={route.path} activeClassName="pf-m-current">
+        {route.label}
+      </NavLink>
+    </NavItem>
+  );
+
+  const renderNavGroup = (group: IAppRouteGroup, groupIndex: number) => (
+    <NavExpandable
+      key={`${group.label}-${groupIndex}`}
+      id={`${group.label}-${groupIndex}`}
+      title={group.label}
+      isActive={group.routes.some((route) => route.path === location.pathname)}
+    >
+      {group.routes.map((route, idx) => route.label && renderNavItem(route, idx))}
+    </NavExpandable>
+  );
+
+  const Navigation = (
+    <Nav id="nav-primary-simple" theme="dark">
+      <NavList id="nav-list-simple">
+        {routes.map(
+          (route, idx) => route.label && (!route.routes ? renderNavItem(route, idx) : renderNavGroup(route, idx))
+        )}
+      </NavList>
+    </Nav>
+  );
+  const Sidebar = (
+    <PageSidebar
+      theme="dark"
+      nav={Navigation}
+      isNavOpen={isMobileView ? isNavOpenMobile : isNavOpen} />
+  );
+  const PageSkipToContent = (
+    <SkipToContent href="#primary-app-container">
+      Skip to Content
+    </SkipToContent>
+  );
+  return (
+    <Page
+      mainContainerId="primary-app-container"
+      header={Header}
+      sidebar={Sidebar}
+      onPageResize={onPageResize}
+      skipToContent={PageSkipToContent}>
+      {children}
+    </Page>
+  );
+}
+
+export { AppLayout };
