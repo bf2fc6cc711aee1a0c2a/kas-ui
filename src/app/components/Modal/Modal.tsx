d894d95d1a650a9de21dc649035ef3bd133b7144
Christie Molloy <cmolloy@redhat.com>
2020-11-20 16:03:40 -0500

    update readme and yaml

--- src/app/components/Modal/Modal.tsx
+++ src/app/components/Modal/Modal.tsx
@@ -1,23 +1,60 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import {
   Button,
-  Title
+  Title,
+  Modal as PFModal,
+  ModalVariant,
+  Form,
+  FormGroup,
+  TextInput,
+  Tile,
+  FormSelect,
+  FormSelectOption,
+  Switch,
+  SplitItem,
+  Split,
 } from '@patternfly/react-core';
 import { KafkaRequestAllOf } from '../../../openapi/api';
 import axios from 'axios';
 import { Services } from '../../common/app-config';
 import { NewKafka } from '../../models/models';
+import { AwsIcon, ExclamationCircleIcon } from '@patternfly/react-icons';
 
 type ModalProps = {
-  mainToggle: boolean
-}
+  createStreamsInstance: boolean;
+  setCreateStreamsInstance: z(createStreamsInstance: boolean) => void;
+  mainToggle: boolean;
+};
 
-
+type InstanceFormData = {
+  instanceName?: string;
+  cloudProvider: string;
+  cloudRegion: string;
+  isMultipleZonesAvailable: boolean;
+};
 // const fetchKafkas = async () => {
 //   await apisService.listKafkas()
+const cloudRegionOptions = [
+  { value: '', label: 'Please Choose', disabled: false },
+  { value: 'us-east', label: 'US East, N. Virginia', disabled: false },
+];
 
-
-const Modal = ({mainToggle}: ModalProps) => {
+const Modal: React.FunctionComponent<ModalProps> = ({
+  createStreamsInstance,
+  setCreateStreamsInstance,
+  mainToggle,
+}: ModalProps) => {
+  const [instanceFormData, setInstanceFormData] = useState<InstanceFormData>({
+    cloudProvider: 'aws',
+    cloudRegion: 'us-east',
+    isMultipleZonesAvailable: false,
+  });
+  const [nameValidated, setNameValidated] = useState<'success' | 'warning' | 'error' | 'default' | undefined>(
+    'default'
+  );
+  const [cloudRegionValidated, setCloudRegionValidated] = useState<
+    'success' | 'warning' | 'error' | 'default' | undefined
+  >('default');
 
   const apisService = Services.getInstance().apiService;
 
@@ -26,28 +63,135 @@ const Modal = ({mainToggle}: ModalProps) => {
 
     // Update this to use the values from the event
     const newKafka: NewKafka = new NewKafka();
-    newKafka.name = "test";
+    newKafka.name = 'test';
     newKafka.multi_az = false;
-    newKafka.owner = "test";
-    newKafka.region = "test";
-    newKafka.cloud_provider = "test";
+    newKafka.owner = 'test';
+    newKafka.region = 'test';
+    newKafka.cloud_provider = 'test';
 
-    await apisService.createKafka(true, newKafka).then(res => {
-      console.info("Kafka was successfully created")
-      // TO DO: User needs to know what Kafka was successfully created
-    })
-    .catch(error => {
-      console.error("Error creating Kafka")
-      // TO DO: Set up error handling
-    })
-  }
+    await apisService
+      .createKafka(true, newKafka)
+      .then((res) => {
+        console.info('Kafka was successfully created');
+        handleModalToggle();
+        // TO DO: User needs to know what Kafka was successfully created
+      })
+      .catch((error) => {
+        console.error('Error creating Kafka');
+        // TO DO: Set up error handling
+      });
+  };
 
+  const handleModalToggle = () => {
+    setCreateStreamsInstance(!createStreamsInstance);
+  };
+
+  const handleInstanceNameChange = (name?: string) => {
+    setInstanceFormData({ ...instanceFormData, instanceName: name });
+    setNameValidated(
+      name === undefined || name === '' ? 'error' : /^[a-zA-Z0-9][a-zA-Z0-9 ]*$/.test(name) ? 'success' : 'error'
+    );
+  };
+  const handleCloudRegionChange = (region: string, _event: any) => {
+    setCloudRegionValidated(region === undefined || region === '' ? 'error' : 'success');
+    setInstanceFormData({ ...instanceFormData, cloudRegion: region });
+  };
+  const handleMultipleZoneSwitch = () => {
+    setInstanceFormData({ ...instanceFormData, isMultipleZonesAvailable: !instanceFormData.isMultipleZonesAvailable });
+  };
+
+  const isFormValid =
+    instanceFormData &&
+    instanceFormData.instanceName &&
+    nameValidated !== 'error' &&
+    instanceFormData.cloudProvider &&
+    instanceFormData.cloudRegion &&
+    instanceFormData.cloudRegion !== '';
   return (
     <>
-    <Title headingLevel="h1" size="lg">Modal goes here</Title>
-    <Button variant="primary" onClick={onCreateInstance}>Create instance</Button>
+      <PFModal
+        variant={ModalVariant.large}
+        title="Create a Streams Instance"
+        isOpen={createStreamsInstance}
+        onClose={handleModalToggle}
+        actions={[
+          <Button key="create" variant="primary" onClick={onCreateInstance} isDisabled={!isFormValid}>
+            Create instance
+          </Button>,
+          <Button key="cancel" variant="link" onClick={handleModalToggle}>
+            Cancel
+          </Button>,
+        ]}
+      >
+        <Form>
+          <FormGroup
+            label="Instance name"
+            helperTextInvalid="This is required field"
+            helperTextInvalidIcon={<ExclamationCircleIcon />}
+            isRequired
+            validated={nameValidated}
+            fieldId="form-instance-name"
+          >
+            <TextInput
+              isRequired
+              type="text"
+              id="form-instance-name"
+              name="form-instance-name"
+              value={instanceFormData?.instanceName}
+              onChange={handleInstanceNameChange}
+            />
+          </FormGroup>
+          <FormGroup label="Cloud Provider" fieldId="form-cloud-provider-name">
+            <Tile
+              title=""
+              isSelected={instanceFormData.cloudProvider === 'aws'}
+              onClick={() => setInstanceFormData({ ...instanceFormData, cloudProvider: 'aws' })}
+            >
+              <Split hasGutter>
+                <SplitItem>
+                  <AwsIcon size="xl" />
+                </SplitItem>
+                <SplitItem>
+                  <Title headingLevel="h3" size="lg">
+                    Amazon Web Services
+                  </Title>
+                </SplitItem>
+              </Split>
+            </Tile>
+          </FormGroup>
+          <FormGroup
+            label="Cloud Region"
+            helperTextInvalid="This is required field"
+            helperTextInvalidIcon={<ExclamationCircleIcon />}
+            validated={cloudRegionValidated}
+            fieldId="form-cloud-region-option"
+          >
+            <FormSelect
+              value={instanceFormData.cloudRegion}
+              onChange={handleCloudRegionChange}
+              id="cloud-region-select"
+              name="cloud-region"
+              aria-label="Cloud region"
+            >
+              {cloudRegionOptions.map((option, index) => (
+                <FormSelectOption isDisabled={option.disabled} key={index} value={option.value} label={option.label} />
+              ))}
+            </FormSelect>
+          </FormGroup>
+          <FormGroup label="Multiple availabilty zones" fieldId="form-cloud-provider-name">
+            <Switch
+              id="multiple-zone-avail-switch"
+              aria-label="multiple avialabilty zones"
+              isChecked={instanceFormData.isMultipleZonesAvailable}
+              onClick={handleMultipleZoneSwitch}
+            />
+          </FormGroup>
+        </Form>
+        <br />
+        <br />
+      </PFModal>
     </>
   );
-}
+};
 
 export { Modal };
