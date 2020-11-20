4fd64ac46fbe49760ed761c82f0959b321027f7d
Christie Molloy <cmolloy@redhat.com>
2020-11-20 16:50:14 -0500

    add delete function

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Title } from '@patternfly/react-core';
 import { KafkaRequestAllOf } from '../../../openapi/api';
+import { Services } from '../../common/app-config';
 
 type TableProps = {
   kafkaInstanceItems: KafkaRequestAllOf,
@@ -8,6 +9,18 @@ type TableProps = {
 }
 
 const Table = ({mainToggle, kafkaInstanceItems}: TableProps) => {
+
+  const apisService = Services.getInstance().apiService;
+
+  const onDeleteInstance = async (event) => {
+    await apisService.deleteKafkaById("").then(res => {
+      console.info("Kafka successfully deleted")
+    })
+    .catch(error => {
+      console.log("Error deleting Kafka")
+    })
+  }
+
   return (
     <>
       <Title headingLevel="h1" size="lg">Table goes here</Title>
