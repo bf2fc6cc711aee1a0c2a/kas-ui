aa525a9129d7f554bc7c282a580089ca26a0bd74
ajaypratap003 <ajay.pratap233@gmail.com>
2020-11-23 15:39:48 +0530

    create instance table and display data

--- src/app/components/Table/Table.tsx
+++ src/app/components/Table/Table.tsx
@@ -1,17 +1,87 @@
-import * as React from 'react';
-import { Title } from '@patternfly/react-core';
+import  React from 'react';
+import {
+  Table as PFTable,
+  TableVariant,
+  TableHeader,
+  TableBody,
+  IRowData
+} from '@patternfly/react-table';
 import { KafkaRequestAllOf } from '../../../openapi/api';
+import {StatusColumn} from './StatusColumn';
+import {InstanceStatus} from '@app/constants';
 
 type TableProps = {
-  kafkaInstanceItems: KafkaRequestAllOf,
+  kafkaInstanceItems: KafkaRequestAllOf[],
   mainToggle: boolean
 }
 
 const Table = ({mainToggle, kafkaInstanceItems}: TableProps) => {
+  const tableColumns = [
+    "Name",
+    "Cloud provider",
+    "Region",
+    "Status"    
+  ];
+
+  const getActionResolver = (
+    rowData: IRowData,
+    onDelete: Function
+  ) => {    
+    const {originalData}=rowData;
+    const title=originalData?.status===InstanceStatus.ACCEPTED? "Cancel instance":"Delete instance";
+    return [         
+      {
+        title,
+        id: "delete-instance",
+        onClick: () => onDelete(originalData)
+      }
+    ];
+  };
+
+  const preparedTableCells = (row:any) => {
+    const {name,cloud_provider,region,status}=row;
+    const tableRow= {
+      cells: [
+        name,
+        cloud_provider,
+        region,
+        {
+          title:<StatusColumn status={status}/>
+        } 
+      ],
+      originalData: row     
+    };
+    return tableRow;
+  };
+
+  const tableRows = kafkaInstanceItems.map(preparedTableCells);
+
+  const onDeleteInstance=()=>{
+    /**
+     * Todo: delete instance functionality
+     */
+  }
+
+  const actionResolver = (rowData: IRowData) => {
+    return getActionResolver(
+      rowData,
+      onDeleteInstance
+      );
+  };
+
   return (
-    <>
-      <Title headingLevel="h1" size="lg">Table goes here</Title>
-    </>
+      <>
+      <PFTable
+        variant={TableVariant.compact}
+        cells={tableColumns}
+        rows={tableRows}
+        aria-label="cluster instance list"     
+        actionResolver={actionResolver}        
+      >
+        <TableHeader />
+        <TableBody />
+      </PFTable>
+      </>
   )
 }
 
