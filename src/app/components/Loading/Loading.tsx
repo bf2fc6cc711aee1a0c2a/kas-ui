eaf5aa29b383685a11357cb32e66522c7ed84e17
Christie Molloy <cmolloy@redhat.com>
2020-11-30 15:31:58 -0500

    change import

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Loading.css';
-import { Spinner } from '@redhat-cloud-services/frontend-components/components/esm/spinner';
+import { Spinner } from '@redhat-cloud-services/frontend-components/components/spinner';
 
 export const Loading = () => {
   return (
