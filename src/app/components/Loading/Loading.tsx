6acfdfe4fa37a3f854b71bbe6beb0a912d91cf81
Christie Molloy <cmolloy@redhat.com>
2020-11-30 11:57:54 -0500

    import cloud dot component

---
+++ src/app/components/Loading/Loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Loading.css';
+import { Spinner } from '@redhat-cloud-services/frontend-components/components/esm/spinner';
+
+export const Loading = () => {
+  return (
+   <Spinner/>
+  )
+};
