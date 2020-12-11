09d955f812cfe423abd32f1b27ffefd642474aa1
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-12-11 16:26:18 +0530

    remove page section and revering css changes

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -4,9 +4,7 @@ import { Spinner } from '@redhat-cloud-services/frontend-components';
 import './Loading.css';
 
 export const Loading: React.FunctionComponent = () => (
-  <PageSection>
-    <div className="pf-bullseye-comp">
-        <Spinner />
-    </div>
-  </PageSection>
+  <Bullseye>
+    <Spinner />
+  </Bullseye>
 );
