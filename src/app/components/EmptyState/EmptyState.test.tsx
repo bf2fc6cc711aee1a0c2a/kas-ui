8d65f6a55f30a2ad2e4062fcd38747d2582fe572
Sanjeev Kumar <sanjeevkumar201418@gmail.com>
2020-12-22 13:00:11 +0530

    add useTranslation

--- src/app/components/EmptyState/EmptyState.test.tsx
+++ src/app/components/EmptyState/EmptyState.test.tsx
@@ -5,20 +5,21 @@ import { render, fireEvent } from '@testing-library/react';
 import { I18nextProvider } from 'react-i18next';
 
 jest.mock('react-i18next', () => {
-  const reactI18next = require.requireActual('react-i18next');
+  const reactI18next = jest.requireActual('react-i18next');
   return {
     ...reactI18next,
-    useTranslation: () => ({ t: (key) => key }),
+    useTranslation: () => ({ t: (key) => key, i18n: { changeLanguage: jest.fn() } }),
   };
 });
 
 describe('Empty State Test', () => {
   test('should render empty state component', () => {
     const { getByTestId, getByText } = render(
-      <I18nextProvider i18n={i18n}>
-        <EmptyState createStreamsInstance={false} setCreateStreamsInstance={jest.fn()} mainToggle={false} />
-      </I18nextProvider>
+      <EmptyState createStreamsInstance={false} setCreateStreamsInstance={jest.fn()} mainToggle={false} />
     );
+    getByText('you_do_not_have_any_kafka_instances_yet');
+    getByText('create_a_kafka_instance');
+    getByText('create_a_kafka_instance_to_get_started ');
     getByTestId('empty-state-icon');
     getByTestId('empty-state-title');
     getByTestId('create-kafka-btn');
