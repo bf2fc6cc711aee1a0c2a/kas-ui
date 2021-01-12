59fd991f933f398aa926f43b81920581709bd133
ajaypratap003 <ajay.pratap233@gmail.com>
2021-01-12 15:25:56 +0530

    Implemented PF TextList and TextListItem components

--- src/app/components/CreateInstanceModal/DrawerPanelContentInfo.tsx
+++ src/app/components/CreateInstanceModal/DrawerPanelContentInfo.tsx
@@ -1,30 +1,44 @@
 import React from 'react';
-import { DrawerPanelContent, DrawerHead, TextContent, Text, TextVariants } from '@patternfly/react-core';
+import {
+  DrawerPanelContent,
+  DrawerHead,
+  TextContent,
+  Text,
+  TextVariants,
+  TextList,
+  TextListVariants,
+  TextListItem,
+  TextListItemVariants,
+} from '@patternfly/react-core';
 
 const DrawerPanelContentInfo = () => (
   <DrawerPanelContent className="instance-details-panel">
     <DrawerHead>
       <TextContent>
         <Text component={TextVariants.h2}>Cluster information</Text>
-        <Text component={TextVariants.h3}>Ingress/Egress</Text>
-        <Text component={TextVariants.p}>up to 4 MBps</Text>
-        <Text component={TextVariants.h3}>Storage</Text>
-        <Text component={TextVariants.p}>up to 100 GB</Text>
-        <Text component={TextVariants.h3}>Partitions</Text>
-        <Text component={TextVariants.p}>up to 100</Text>
-        <Text component={TextVariants.h3}>Client connections</Text>
-        <Text component={TextVariants.p}>up to 100</Text>
-        <Text component={TextVariants.h3}>Message size</Text>
-        <Text component={TextVariants.p}>up to 1 MB</Text>
-        <Text component={TextVariants.h3}>Availability</Text>
-        <Text component={TextVariants.p}>Multizone highly available</Text>
+        <TextList component={TextListVariants.dl}>
+          <TextListItem component={TextListItemVariants.dt}>Ingress/Egress</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>up to 4 MBps</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Storage</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>up to 100 GB</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Partitions</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>up to 100</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Client connections</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>up to 500</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Message size</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>up to 1 MB</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Availability</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>Multizone highly available</TextListItem>
+        </TextList>
         <Text component={TextVariants.h2}>Cost information</Text>
-        <Text component={TextVariants.h3}>Base Cluster</Text>
-        <Text component={TextVariants.p}>$1.50/hr</Text>
-        <Text component={TextVariants.h3}>Ingress/Egress</Text>
-        <Text component={TextVariants.p}>$0.02/MB</Text>
-        <Text component={TextVariants.h3}>Storage</Text>
-        <Text component={TextVariants.p}>$0.0002/GB/hr</Text>
+        <TextList component={TextListVariants.dl}>
+          <TextListItem component={TextListItemVariants.dt}>Base Cluster</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>$1.50/hr</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Ingress/Egress</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>$0.02/MB</TextListItem>
+          <TextListItem component={TextListItemVariants.dt}>Storage</TextListItem>
+          <TextListItem component={TextListItemVariants.dd}>$0.0002/GB/hr</TextListItem>
+        </TextList>
       </TextContent>
     </DrawerHead>
   </DrawerPanelContent>
