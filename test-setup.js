409cb43970887b1074b335adaa5827c6dd239acf
ajaypratap003 <ajay.pratap233@gmail.com>
2021-01-15 14:55:22 +0530

    Fixed failed unit tests

--- test-setup.js
+++ test-setup.js
@@ -2,4 +2,4 @@ import { configure } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import "@testing-library/jest-dom/extend-expect";
 
-configure({ adapter: new Adapter(), testIdAttribute: 'id' });
+configure({ adapter: new Adapter() });
