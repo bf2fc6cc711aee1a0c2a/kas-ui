96336579a22c37265b663cc2e3bdba1058e0087b
ajaypratap003 <ajay.pratap233@gmail.com>
2021-01-21 18:59:30 +0530

    Replace Spinner with PF Spinner

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { PageSection, Bullseye } from '@patternfly/react-core';
-import { Spinner } from '@redhat-cloud-services/frontend-components';
-import './Loading.css';
+import { Bullseye, Spinner } from '@patternfly/react-core';
+// import { Spinner } from '@redhat-cloud-services/frontend-components';
+// import './Loading.css';
 
 export const Loading: React.FunctionComponent = () => (
   <Bullseye>
