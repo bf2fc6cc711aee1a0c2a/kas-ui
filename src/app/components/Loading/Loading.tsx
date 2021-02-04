ac9a334e6db8483f41d9e3fdd523d803a45e5ad2
ajaypratap003 <ajay.pratap233@gmail.com>
2021-02-04 22:02:01 +0530

    removed unused imports

--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import { Bullseye, Spinner } from '@patternfly/react-core';
-// import { Spinner } from '@redhat-cloud-services/frontend-components';
-// import './Loading.css';
 
 export const Loading: React.FunctionComponent = () => (
   <Bullseye>
