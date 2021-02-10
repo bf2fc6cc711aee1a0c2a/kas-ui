8059bd1f6411a9d600c4a905172e9abf3125c51a
Pete Muir <pmuir@bleepbleep.org.uk>
2021-02-10 10:41:08 +0000

    feat: initial cut at metrics

--- src/app/Drawer/InstanceDrawer.tsx
+++ src/app/Drawer/InstanceDrawer.tsx
@@ -1,30 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
+  AlertVariant,
   Button,
   Card,
   CardBody,
+  CardHeader,
   ClipboardCopy,
-  DrawerPanelContent,
-  DrawerHead,
-  DrawerPanelBody,
   DrawerActions,
   DrawerCloseButton,
+  DrawerHead,
+  DrawerPanelBody,
+  DrawerPanelContent,
   Flex,
   FlexItem,
   Grid,
   GridItem,
-  Tabs,
   Tab,
+  Tabs,
   TabTitleText,
-  TextContent,
   Text,
-  TextVariants,
+  TextContent,
   TextList,
   TextListItem,
-  TextListVariants,
   TextListItemVariants,
+  TextListVariants,
+  TextVariants,
   Title,
-  TitleSizes,
+  TitleSizes
 } from '@patternfly/react-core';
 import { CopyIcon } from '@patternfly/react-icons';
 import '@patternfly/react-styles/css/utilities/Spacing/spacing.css';
@@ -32,10 +34,16 @@ import '@patternfly/react-styles/css/utilities/Alignment/alignment.css';
 import './InstanceDrawer.css';
 import { GenerateCredential } from './GenerateCredential';
 import { Loading } from '@app/components/Loading/Loading';
-import { KafkaRequest } from 'src/openapi';
+import { DefaultApi, KafkaRequest } from 'src/openapi';
 import dayjs from 'dayjs';
 import localizedFormat from 'dayjs/plugin/localizedFormat';
 import { useTranslation } from 'react-i18next';
+import { isServiceApiError } from '@app/utils';
+import { AuthContext } from '@app/auth/AuthContext';
+import { ApiContext } from '@app/api/ApiContext';
+import { useAlerts } from '@app/components/Alerts/Alerts';
+import { AvailableDiskSpaceChart, Broker } from '@app/Drawer/AvailableDiskSpaceChart';
+import { MessagesChart, Topic } from '@app/Drawer/MessagesChart';
 
 export type InstanceDrawerProps = {
   mainToggle: boolean;
@@ -45,12 +53,21 @@ export type InstanceDrawerProps = {
   activeTab?: 'Details' | 'Connection';
 };
 const InstanceDrawer: React.FunctionComponent<InstanceDrawerProps> = ({
-  mainToggle,
-  onClose,
-  activeTab,
-  instanceDetail,
-}) => {
+                                                                        mainToggle,
+                                                                        onClose,
+                                                                        activeTab,
+                                                                        instanceDetail
+                                                                      }) => {
+  const authContext = useContext(AuthContext);
+  const { basePath } = useContext(ApiContext);
+  const { addAlert } = useAlerts();
   const { t } = useTranslation();
+
+  const [globalPartitionCount, setGlobalPartitionCount] = useState<number>(0);
+  const [offlinePartitionCount, setOfflinePartitionCount] = useState<number>(0);
+  const [brokerAvailableSpace, setBrokerAvailableSpace] = useState<Broker[]>([] as Broker[]);
+  const [topicMessageCount, setTopicMessageCount] = useState<Topic[]>([] as Topic[]);
+
   const { id, created_at, updated_at, owner } = instanceDetail || {};
   dayjs.extend(localizedFormat);
 
@@ -72,6 +89,146 @@ const InstanceDrawer: React.FunctionComponent<InstanceDrawerProps> = ({
   const externalServer = instanceDetail?.bootstrapServerHost?.endsWith(':443')
     ? instanceDetail?.bootstrapServerHost
     : `${instanceDetail?.bootstrapServerHost}:443`;
+
+  // Functions
+  const fetchInstantMetrics = async () => {
+    const accessToken = await authContext?.getToken();
+    if (accessToken !== undefined && accessToken !== '') {
+      try {
+        const apisService = new DefaultApi({
+          accessToken,
+          basePath
+        });
+        if (!instanceDetail || !instanceDetail.id) {
+          return;
+        }
+        await apisService.getMetricsByKafkaId(instanceDetail.id, 1, 5, ['kafka_controller_kafkacontroller_offline_partitions_count', 'kafka_controller_kafkacontroller_global_partition_count']).then((res) => {
+          const data = res.data;
+          let globalPartitionCounter = 0, offlinePartitionCounter = 0;
+          data.items?.forEach(item => {
+            const labels = item.metric;
+            if (labels === undefined) {
+              throw new Error('item.metric cannot be undefined');
+            }
+            if (item.values === undefined) {
+              throw new Error('item.values cannot be undefined');
+            }
+            if (labels['__name__'] === 'kafka_controller_kafkacontroller_offline_partitions_count') {
+              offlinePartitionCounter += item.values[item.values.length - 1].Value;
+            }
+            if (labels['__name__'] === 'kafka_controller_kafkacontroller_global_partition_count') {
+              globalPartitionCounter += item.values[item.values.length - 1].Value;
+            }
+          });
+          setGlobalPartitionCount(globalPartitionCounter);
+          setOfflinePartitionCount(offlinePartitionCounter);
+        });
+      } catch (error) {
+        let reason: string | undefined;
+        if (isServiceApiError(error)) {
+          reason = error.response?.data.reason;
+        }
+        /**
+         * Todo: show user friendly message according to server code
+         * and translation for specific language
+         *
+         */
+        addAlert(t('something_went_wrong'), AlertVariant.danger, reason);
+      }
+    }
+  };
+
+  // Functions
+  const fetchMetrics = async () => {
+    const accessToken = await authContext?.getToken();
+    if (accessToken !== undefined && accessToken !== '') {
+      try {
+        const apisService = new DefaultApi({
+          accessToken,
+          basePath
+        });
+        if (!instanceDetail || !instanceDetail.id) {
+          return;
+        }
+        await apisService.getMetricsByKafkaId(instanceDetail.id, 6 * 60, 5 * 60, ['kubelet_volume_stats_available_bytes', 'kafka_server_brokertopicmetrics_messages_in_total']).then((res) => {
+          const data = res.data;
+          const brokers = [] as Broker[];
+          const topics = [] as Topic[];
+          data.items?.forEach((item, i) => {
+            const labels = item.metric;
+            if (labels === undefined) {
+              throw new Error('item.metric cannot be undefined');
+            }
+            if (item.values === undefined) {
+              throw new Error('item.values cannot be undefined');
+            }
+            if (labels['__name__'] === 'kubelet_volume_stats_available_bytes') {
+              const pvcName = labels['persistentvolumeclaim'];
+              if (!pvcName.includes('zookeeper')) {
+                const broker = {
+                  name: `broker ${i}`,
+                  data: []
+                } as Broker;
+                item.values?.forEach(value => {
+                  if (value.Timestamp == undefined) {
+                    throw new Error('timestamp cannot be undefined');
+                  }
+                  const hardLimit = 225 * 1024 * 1024 * 1024 * .95;
+                  const usedSpaceInBytes = hardLimit - value.Value;
+                  const softLimit = 225 * 1024 * 1024 * 1024 * .90;
+                  broker.data.push({
+                    timestamp: value.Timestamp,
+                    usedSpace: usedSpaceInBytes,
+                    hardLimit,
+                    softLimit
+                  });
+                });
+                brokers.push(broker);
+              }
+            }
+            if (labels['__name__'] === 'kafka_server_brokertopicmetrics_messages_in_total') {
+              const name = labels['topic'];
+              if (name !== '__consumer_offsets' && name !== 'strimzi-canary') {
+                let topic = {
+                  name,
+                  data: []
+                } as Topic;
+
+                item.values.forEach(value => {
+                  if (value.Timestamp == undefined) {
+                    throw new Error('timestamp cannot be undefined');
+                  }
+                  topic.data.push({
+                    count: value.Value,
+                    timestamp: value.Timestamp
+                  });
+                });
+                topics.push(topic);
+              }
+            }
+          });
+          setTopicMessageCount(topics);
+          setBrokerAvailableSpace(brokers);
+        });
+      } catch (error) {
+        let reason: string | undefined;
+        if (isServiceApiError(error)) {
+          reason = error.response?.data.reason;
+        }
+        /**
+         * Todo: show user friendly message according to server code
+         * and translation for specific language
+         *
+         */
+        addAlert(t('something_went_wrong'), AlertVariant.danger, reason);
+      }
+    }
+  };
+
+  useEffect(() => {
+    fetchMetrics();
+    fetchInstantMetrics();
+  }, [instanceDetail?.id]);
 
   const resourcesTab = (
     <>
@@ -170,41 +327,90 @@ const InstanceDrawer: React.FunctionComponent<InstanceDrawerProps> = ({
     </>
   );
 
+  const AvailableSpaceChart = () => {
+    if (brokerAvailableSpace.length > 0) {
+      return <AvailableDiskSpaceChart brokers={brokerAvailableSpace} />;
+    } else {
+      return <></>;
+    }
+  };
+
+  const ConnectedMessagesChart = () => {
+    if (topicMessageCount.length > 0) {
+      return <MessagesChart topics={topicMessageCount} />;
+    } else {
+      return <></>;
+    }
+  };
+
   const detailsTab = (
     <>
       <div className="mk--instance-details__drawer--tab-content">
-        {mainToggle && (
-          <Grid className="mk--instance-details__drawer--grid">
-            <GridItem span={6} className="mk--instance-details__drawer--grid--column-one">
-              <Card isFlat>
-                <CardBody>
-                  <TextContent>
+        <Grid className="mk--instance-details__drawer--grid">
+          <GridItem span={6} className="mk--instance-details__drawer--grid--column-one">
+            <Card isFlat>
+              <CardBody>
+                <TextContent>
+                  <Text component={TextVariants.small} className="pf-u-mb-0">
+                    {t('topics')}
+                  </Text>
+                  <Text component={TextVariants.h3} className="pf-u-mt-0">
+                    10
+                  </Text>
+                </TextContent>
+              </CardBody>
+            </Card>
+          </GridItem>
+          <GridItem span={6}>
+            <Card isFlat>
+              <CardBody>
+                <TextContent>
+                  <Text component={TextVariants.small} className="pf-u-mb-0">
+                    {t('consumer_groups')}
+                  </Text>
+                  <Text component={TextVariants.h3} className="pf-u-mt-0">
+                    8
+                  </Text>
+                </TextContent>
+              </CardBody>
+            </Card>
+          </GridItem>
+          <GridItem span={6} className="mk--instance-details__drawer--grid--column-one pf-u-mt-sm">
+            <Card isFlat>
+              <CardHeader>
+                <TextContent>
+                  <Text component={TextVariants.small} className="pf-u-mb-0">
+                    {t('topic_partitions')}
+                  </Text>
+                </TextContent>
+              </CardHeader>
+              <CardBody>
+                <Grid>
+                  <GridItem span={6}>
                     <Text component={TextVariants.small} className="pf-u-mb-0">
-                      {t('topics')}
+                      {t('topic_partitions_online')}
                     </Text>
+                  </GridItem>
+                  <GridItem span={6}>
                     <Text component={TextVariants.h3} className="pf-u-mt-0">
-                      10
+                      {globalPartitionCount - offlinePartitionCount}
                     </Text>
-                  </TextContent>
-                </CardBody>
-              </Card>
-            </GridItem>
-            <GridItem span={6}>
-              <Card isFlat>
-                <CardBody>
-                  <TextContent>
+                  </GridItem>
+                  <GridItem span={6}>
                     <Text component={TextVariants.small} className="pf-u-mb-0">
-                      {t('consumer_groups')}
+                      {t('topic_partitions_offline')}
                     </Text>
+                  </GridItem>
+                  <GridItem span={6}>
                     <Text component={TextVariants.h3} className="pf-u-mt-0">
-                      8
+                      {offlinePartitionCount}
                     </Text>
-                  </TextContent>
-                </CardBody>
-              </Card>
-            </GridItem>
-          </Grid>
-        )}
+                  </GridItem>
+                </Grid>
+              </CardBody>
+            </Card>
+          </GridItem>
+        </Grid>
         <TextContent>
           <TextList component={TextListVariants.dl}>
             {renderTextListItemDetail(t('cloud_provider'), t('amazon_web_services'))}
@@ -215,6 +421,8 @@ const InstanceDrawer: React.FunctionComponent<InstanceDrawerProps> = ({
             {renderTextListItemDetail(t('updated'), dayjs(updated_at).format('LLLL'))}
           </TextList>
         </TextContent>
+        <AvailableSpaceChart />
+        <ConnectedMessagesChart />
       </div>
     </>
   );
--- src/app/OpenshiftStreams/OpenshiftStreams.tsx
+++ src/app/OpenshiftStreams/OpenshiftStreams.tsx
@@ -43,7 +43,7 @@ const OpenshiftStreams = ({ onConnectToInstance, getConnectToInstancePath }: Ope
   const searchParams = new URLSearchParams(location.search);
   const page = parseInt(searchParams.get('page') || '', 10) || 1;
   const perPage = parseInt(searchParams.get('perPage') || '', 10) || 10;
-  const mainToggle = searchParams.has('user-testing');
+  const mainToggle = true;
 
   const { t } = useTranslation();
   const { addAlert } = useAlerts();
