d79bf70af88bd8ff599d10c901cada518606624a
ajaypratap003 <ajay.pratap233@gmail.com>
2021-05-19 17:43:48 +0530

    update openapi yml file path

--- mas-mock/scripts/pullapi.sh
+++ mas-mock/scripts/pullapi.sh
@@ -1,10 +1,10 @@
 
-echo "Synchronizing managed-services-api"
+echo "Synchronizing kas-fleet-manager"
 
-wget --no-check-certificate https://gitlab.cee.redhat.com/service/managed-services-api/-/raw/master/openapi/kas-fleet-manager.yaml
+wget --no-check-certificate https://gitlab.cee.redhat.com/service/kas-fleet-manager/-/raw/master/openapi/kas-fleet-manager.yaml
 mv kas-fleet-manager.yaml ./kas-fleet-manager.yaml
 
 ## Copy api to mock
 cp ./openapi/kas-fleet-manager.yaml ./mas-mock/kas-fleet-manager.yaml
 
-echo "Finished synchronization with managed-services-api"
+echo "Finished synchronization with kas-fleet-manager"
