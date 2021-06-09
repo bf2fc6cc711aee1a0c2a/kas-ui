43bee12b99ea96d0009cfbc61e39a43e98c7bc4d
Ajay Pratap <ajaypratap003@users.noreply.github.com>
2021-06-09 15:43:58 +0530

    added css utilities (#576)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,15 @@ import '@patternfly/patternfly/patternfly.min.css';
 import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
 import '@patternfly/patternfly/utilities/Sizing/sizing.css';
 import '@patternfly/patternfly/utilities/Spacing/spacing.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
 import { AppLayout } from '@app/AppLayout/AppLayout';
 import { AppRoutes } from '@app/routes';
 import '@app/app.css';
 import { getKeycloakInstance } from './auth/keycloak/keycloakAuth';
 import { MASLoading } from '@app/common';
 import { KeycloakAuthProvider, KeycloakContext } from '@app/auth/keycloak/KeycloakContext';
-import kasi18n, { initI18N } from '@i18n/i18n';
+import { initI18N } from '@i18n/i18n';
 import { MASErrorBoundary } from '@app/common';
 
 let keycloak: Keycloak.KeycloakInstance | undefined;
