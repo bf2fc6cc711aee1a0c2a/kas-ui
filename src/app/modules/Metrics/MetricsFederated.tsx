c597863c34093e07e28b57b7abb00089de6131ca
Anukriti Jha <anukritijha@gmail.com>
2021-07-13 14:40:15 +0530

    Remove RootModal, AlertProvider and BrowserRouter

--- src/app/modules/Metrics/MetricsFederated.tsx
+++ src/app/modules/Metrics/MetricsFederated.tsx
@@ -10,15 +10,10 @@ import { I18nextProvider } from 'react-i18next';
 const MetricsFederated: React.FunctionComponent<MetricsProps> = ({ kafkaId }) => {
   return (
     // TODO don't add BrowserRouter here - see  https://github.com/bf2fc6cc711aee1a0c2a/mk-ui-frontend/issues/74
-    <BrowserRouter>
-      <I18nextProvider i18n={initI18N()}>
-        <AlertProvider>
-          <RootModal>
-            <Metrics kafkaId={kafkaId} />
-          </RootModal>
-        </AlertProvider>
-      </I18nextProvider>
-    </BrowserRouter>
+
+    <I18nextProvider i18n={initI18N()}>
+      <Metrics kafkaId={kafkaId} />
+    </I18nextProvider>
   );
 };
 
