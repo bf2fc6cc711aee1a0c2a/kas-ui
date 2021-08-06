d43adf3f281f5c197bb8ded3a313798ad9988e32
Pete Muir <pmuir@bleepbleep.org.uk>
2021-08-06 14:30:14 +0100

    fix: convert open check to promise (#704)

--- src/app/models/federated.ts
+++ src/app/models/federated.ts
@@ -3,12 +3,8 @@ import React, { useContext } from 'react';
 export type FederatedProps = {
   tokenEndPointUrl: string;
   preCreateInstance?: (isOpen: boolean) => Promise<boolean>;
-  shouldOpenCreateModal?: boolean;
+  shouldOpenCreateModal: () => Promise<boolean>;
 };
 
-const initialState: FederatedProps = {
-  tokenEndPointUrl: '',
-};
-
-export const FederatedContext = React.createContext<FederatedProps>(initialState);
-export const useFederated = (): FederatedProps => useContext(FederatedContext);
+export const FederatedContext = React.createContext<FederatedProps | undefined>(undefined);
+export const useFederated = (): FederatedProps | undefined => useContext(FederatedContext);
