ff9ca93407785c3d7c7efdaa6c805ec82ca3fc57
ajaypratap003 <ajay.pratap233@gmail.com>
2021-08-10 13:26:25 +0530

    fix test

--- test-utils/i18n.ts
+++ test-utils/i18n.ts
@@ -1,39 +1,33 @@
 import i18n from 'i18next';
-import { initReactI18next }
-    from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
 import 'dayjs/locale/ja';
 import dayjs from 'dayjs';
 import en from '../src/locales/en/public.json';
-import ja from '../src/locales/ja/public.json';
 
-i18n
-    .use(initReactI18next)
-    .init({
-        lng: 'en',
-        fallbackLng: 'en',        
-        ns: ['translations'],
-        defaultNS: 'translations',
-        debug: true,
-        interpolation: {
-            escapeValue: false, // not needed for react!!
+i18n.use(initReactI18next).init(
+  {
+    lng: 'en',
+    fallbackLng: 'en',
+    ns: ['translations'],
+    defaultNS: 'translations',
+    debug: true,
+    interpolation: {
+      escapeValue: false, // not needed for react!!
+    },
+    resources: {
+      en: {
+        translations: {
+          ...en,
         },
-        resources: {
-            en: {
-                translations: {
-                    ...en
-                }
-            },
-            ja: {
-                translations: {
-                    ...ja
-                }
-            }
-        }
-    }, () => {
-        dayjs.locale(i18n.language);
-    });
-i18n.on('languageChanged', function (lng) {   
-    dayjs.locale(lng);
+      },
+    },
+  },
+  () => {
+    dayjs.locale(i18n.language);
+  }
+);
+i18n.on('languageChanged', function (lng) {
+  dayjs.locale(lng);
 });
 
 export default i18n;
