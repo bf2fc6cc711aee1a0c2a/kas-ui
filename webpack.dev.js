bcb041b77fc5d4d6cbc3f9c8091a60d940bdf032
Pete Muir <pmuir@bleepbleep.org.uk>
2021-08-23 09:05:36 +0100

    chore: fixup deps (#724)
    * chore: fixup deps
    
    * Fix: use correct versions of patternfly
    
    * Fix: change webpack dev server for 4.0.0

--- webpack.dev.js
+++ webpack.dev.js
@@ -12,15 +12,17 @@ module.exports = merge(common('development'), {
   mode: "development",
   devtool: "eval-source-map",
   devServer: {
-    contentBase: "./dist",
+    static: {
+      directory: "./dist",
+    },
+    client: {
+      overlay: true
+    },
     host: HOST,
     port: PORT,
-    compress: true,
-    inline: true,
     historyApiFallback: true,
     hot: true,
-    overlay: true,
-    open: true,
+    //open: true,
     https: PROTOCOL === "https",
     headers: {
       "Access-Control-Allow-Origin": "*",
