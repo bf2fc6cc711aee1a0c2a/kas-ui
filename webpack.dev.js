f306968172b31e4f84997493c709dee3e2a30497
Pete Muir <pmuir@bleepbleep.org.uk>
2021-09-21 21:07:30 +0100

    Fix: move to non-broken version of ui-shared
    * also resolve ws issue in dev-server
    * move the prettier ignore to the right place

--- webpack.dev.js
+++ webpack.dev.js
@@ -30,6 +30,7 @@ module.exports = merge(common('development'), {
       'Access-Control-Allow-Headers':
         'X-Requested-With, content-type, Authorization',
     },
+    allowedHosts: 'all',
   },
   plugins: [
     new CopyPlugin({
