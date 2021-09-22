3fed3e5b8ceafc6aa88f558becaec3a3d92181de
Cliodhna Fullam <cfullam@redhat.com>
2021-09-22 09:25:47 +0100

    added time updates

--- src/app/modules/Metrics/components/LogSizePerPartitionChart/LogSizePerPartitionChart.tsx
+++ src/app/modules/Metrics/components/LogSizePerPartitionChart/LogSizePerPartitionChart.tsx
@@ -266,7 +266,7 @@ export const LogSizePerPartitionChart: React.FC<KafkaInstanceProps> = ({
             ) : (
               <ChartEmptyState
                 title="No metrics data"
-                body="Your Kafka instance was recently created, so some metrics aren’t yet available. Check back in a few minutes."
+                body="Your Kafka instance was recently created, so some metrics aren’t yet available. Metrics should be available around 30 minutes after creation."
                 noData
               />
             )
