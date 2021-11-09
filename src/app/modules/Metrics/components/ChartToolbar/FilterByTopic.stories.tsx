c878b2b0f9c94a22762dcec5182f6d6b07ae2249
Riccardo Forina <rforina@redhat.com>
2021-11-09 12:41:43 +0100

    chore: fix FilterByTopic rendering issue

--- src/app/modules/Metrics/components/ChartToolbar/FilterByTopic.stories.tsx
+++ src/app/modules/Metrics/components/ChartToolbar/FilterByTopic.stories.tsx
@@ -1,14 +1,14 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import React from 'react';
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
 
-import { FilterByTopic } from './FilterByTopic';
+import { FilterByTopic } from "./FilterByTopic";
 
 export default {
-  title: 'Metrics/Components/FilterByTopic',
+  title: "Metrics/Components/FilterByTopic",
   component: FilterByTopic,
   args: {
     selectedTopic: undefined,
-    topicList: ['lorem', 'dolor', 'ipsum'],
+    topicList: ["lorem", "dolor", "ipsum"],
     disableToolbar: false,
   },
 } as ComponentMeta<typeof FilterByTopic>;
@@ -17,5 +17,20 @@ const Template: ComponentStory<typeof FilterByTopic> = (args) => (
   <FilterByTopic {...args} />
 );
 
-export const Story = Template.bind({});
-Story.args = {};
+export const Default = Template.bind({});
+Default.args = {};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disableToolbar: true,
+};
+
+export const NoTopics = Template.bind({});
+NoTopics.args = {
+  topicList: undefined,
+};
+
+export const MultipleTopicsWithCommonWords = Template.bind({});
+MultipleTopicsWithCommonWords.args = {
+  topicList: ["lorem dolor", "lorem ipsum", "lorem foo", "dolor", "ipsum"],
+};
--- src/app/modules/Metrics/components/ChartToolbar/FilterByTopic.tsx
+++ src/app/modules/Metrics/components/ChartToolbar/FilterByTopic.tsx
@@ -1,71 +1,68 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from "react";
 import {
   ToolbarItem,
   Select,
   SelectVariant,
   SelectGroup,
   SelectOption,
-} from '@patternfly/react-core';
-import FilterIcon from '@patternfly/react-icons/dist/js/icons/filter-icon';
+} from "@patternfly/react-core";
+import FilterIcon from "@patternfly/react-icons/dist/js/icons/filter-icon";
+
+const widths = {
+  default: "100px",
+  sm: "80px",
+  md: "150px",
+  lg: "200px",
+  xl: "250px",
+  "2xl": "300px",
+};
 
 type FilterByTopicProps = {
-  setSelectedTopic?: (value: string | boolean) => void;
   selectedTopic?: string | boolean;
   topicList?: string[];
-  setIsFilterApplied?: (value: boolean) => void;
   disableToolbar: boolean;
+  onSetIsFilterApplied?: (value: boolean) => void;
+  onSetSelectedTopic?: (value: string | boolean) => void;
 };
 
 export const FilterByTopic = ({
   selectedTopic,
-  setSelectedTopic,
-  topicList,
-  setIsFilterApplied,
+  topicList = [],
   disableToolbar,
+  onSetSelectedTopic,
+  onSetIsFilterApplied,
 }: FilterByTopicProps) => {
   const [isTopicSelectOpen, setIsTopicSelectOpen] = useState<boolean>(false);
-  const [topicListFinal, setTopicListFinal] = useState<string[]>();
-
-  useEffect(() => {
-    topicList && setTopicListFinal(topicList);
-  }, [topicList]);
 
   const onTopicToggle = (isTopicSelectOpen) => {
     setIsTopicSelectOpen(isTopicSelectOpen);
   };
 
   const onTopicSelect = (_, selection) => {
-    setSelectedTopic && setSelectedTopic(selection);
-    setIsFilterApplied &&
-      (selection !== 'All topics'
-        ? setIsFilterApplied(true)
-        : setIsFilterApplied(false));
+    onSetSelectedTopic && onSetSelectedTopic(selection);
+    onSetIsFilterApplied &&
+      (selection !== "All topics"
+        ? onSetIsFilterApplied(true)
+        : onSetIsFilterApplied(false));
     setIsTopicSelectOpen(false);
   };
 
   const onTopicFilter = (_, textInput) => {
     const filteredTopics =
-      topicListFinal?.filter((topic) => topic.indexOf(textInput) != -1) || [];
+      topicList?.filter((topic) => topic.indexOf(textInput) != -1) || [];
     return topicOptions(filteredTopics);
   };
 
   const topicOptions = (topicList) => [
-    <SelectOption key={'topic-filter-0'} value='All topics' />,
-    <SelectGroup label='Filter by topic' key='topic-filter-group'>
+    <SelectOption key={"topic-filter-0"} value="All topics" />,
+    <SelectGroup label="Filter by topic" key="topic-filter-group">
       {topicList?.map((topic, index) => (
         <SelectOption key={`topic-filter-${index + 1}`} value={topic} />
       ))}
     </SelectGroup>,
   ];
 
-  const widths = {
-    default: '100px',
-    sm: '80px',
-    md: '150px',
-    lg: '200px',
-    xl: '250px',
-    '2xl': '300px',
-  };
+  const isDisabled = disableToolbar || topicList.length === 0;
 
   return (
     <ToolbarItem widths={widths}>
@@ -80,14 +77,14 @@ export const FilterByTopic = ({
             <FilterIcon /> All topics
           </>
         }
-        aria-labelledby={'titleId'}
+        aria-labelledby={"titleId"}
         onFilter={onTopicFilter}
         isGrouped
         hasInlineFilter
-        isDisabled={disableToolbar}
-        style={{ width: '100%' }}
+        isDisabled={isDisabled}
+        style={{ width: "100%" }}
       >
-        {topicOptions(topicListFinal)}
+        {topicOptions(topicList)}
       </Select>
     </ToolbarItem>
   );
