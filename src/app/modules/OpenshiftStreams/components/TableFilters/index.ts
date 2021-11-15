86d480b7c2574719ec58b03aaef035a20859d0ae
Pete Muir <pmuir@bleepbleep.org.uk>
2021-11-15 16:39:24 +0000

    fix: properly export the types (#906)

--- src/app/modules/OpenshiftStreams/components/TableFilters/index.ts
+++ src/app/modules/OpenshiftStreams/components/TableFilters/index.ts
@@ -1,3 +1,2 @@
 export * from './StreamsFilterGroup';
-export { FilterType } from '@app/modules/OpenshiftStreams/components/TableFilters/types';
-export { FilterValue } from '@app/modules/OpenshiftStreams/components/TableFilters/types';
+export * from './types';
