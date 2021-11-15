a4de482c75c6de564284399e577408722655afef
hemahg <hhg@redhat.com>
2021-11-15 13:23:08 +0530

    Fixing compilation error while running kas-ui locally

--- src/app/modules/OpenshiftStreams/components/TableFilters/index.ts
+++ src/app/modules/OpenshiftStreams/components/TableFilters/index.ts
@@ -1,3 +1,1 @@
-export * from './StreamsFilterGroup';
-export { FilterType } from '@app/modules/OpenshiftStreams/components/TableFilters/types';
-export { FilterValue } from '@app/modules/OpenshiftStreams/components/TableFilters/types';
+export * from "./StreamsFilterGroup";
