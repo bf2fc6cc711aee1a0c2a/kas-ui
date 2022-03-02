3caf679579a41a9370730e38a41c023eea96ff51
Ajay Pratap <ajaypratap003@users.noreply.github.com>
2022-03-02 12:33:22 +0530

    add error code for max limit kafka and unsupported instancetype (#1072)

--- src/app/utils/error.ts
+++ src/app/utils/error.ts
@@ -5,12 +5,20 @@ import {
 } from '@rhoas/kafka-management-sdk';
 
 const ErrorCodes = {
+  /** Forbidden to perform this action*/
   UNAUTHORIZED_USER: APIErrorCodes.ERROR_4,
+  /** Kafka cluster name is already used*/
   DUPLICATE_INSTANCE_NAME: APIErrorCodes.ERROR_36,
-  PREVIEW_KAFKA_INSTANCE_EXIST: APIErrorCodes.ERROR_24,
+  /** The maximum number of allowed kafka instances has been reached*/
+  REACHED_MAX_LIMIT_ALLOWED_KAFKA: APIErrorCodes.ERROR_24,
+  /** Insufficient quota*/
   INSUFFICIENT_QUOTA: APIErrorCodes.ERROR_120,
+  /** Failed to check quota*/
   FAILED_TO_CHECK_QUOTA: APIErrorCodes.ERROR_121,
+  /** Bad request*/
   OWNER_DOES_NOT_EXIST: APIErrorCodes.ERROR_21,
+  /** Instance Type not supported*/
+  INSTANCE_TYPE_NOT_SUPPORTED: APIErrorCodes.ERROR_41,
 };
 
 export { ErrorCodes, isServiceApiError, getErrorCode };
