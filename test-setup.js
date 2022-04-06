f7e5769a836d3ada1c3945e308b1e56b6d56f5af
Riccardo Forina <rforina@redhat.com>
2022-04-06 22:31:05 +0200

    drop enzyme

--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,2 @@
-import { configure } from '@testing-library/react';
-import Adapter from 'enzyme-adapter-react-16';
-import '@testing-library/jest-dom/extend-expect';
-
-configure({ adapter: new Adapter() });
+import { configure } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
