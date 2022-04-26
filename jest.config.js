f8bd95b5690a724e34305e7872c889d11469646d
Riccardo Forina <rforina@redhat.com>
2022-04-26 09:04:59 +0200

    chore: fix test runner

--- jest.config.js
+++ jest.config.js
@@ -1,30 +1,35 @@
-const merge = require('lodash.merge');
-const { jestModuleMapper } = require('./utils/tooling/aliasHelper');
-const { compilerOptions } = require('./tsconfig.json');
-const commonConfig = require('./test_common/jest.common.config');
+const merge = require("lodash.merge");
+const { jestModuleMapper } = require("./utils/tooling/aliasHelper");
+const { compilerOptions } = require("./tsconfig.json");
+const commonConfig = require("./test_common/jest.common.config");
 
 const config = {
-  transformIgnorePatterns: ['node_modules/(?!byte-size)'],
-  setupFilesAfterEnv: ['<rootDir>/test_common/jest_rtl_setup.ts'],
-  roots: ['./src'],
-  coverageDirectory: '<rootDir>/coverage',
+  transformIgnorePatterns: ["node_modules/(?!byte-size)"],
+  setupFilesAfterEnv: ["<rootDir>/test_common/jest_rtl_setup.ts"],
+  roots: ["./src"],
+  coverageDirectory: "<rootDir>/coverage",
   moduleNameMapper: {
     ...compilerOptions.paths,
     ...jestModuleMapper,
-    '\\.(css|less)$':
-      '<rootDir>/node_modules/@patternfly/react-styles/__mocks__/styleMock.js',
+    "\\.(css|less)$":
+      "<rootDir>/node_modules/@patternfly/react-styles/__mocks__/styleMock.js",
   },
-  testEnvironment: 'jsdom',
+  testEnvironment: "jsdom",
   collectCoverageFrom: [
-    '**/*.{js,ts,jsx,tsx}',
-    '!**/index.{js,ts,jsx,tsx}',
-    '!**/*.steps.*',
-    '!**/*.d.ts',
-    '!**/*types.ts',
-    '!**/*.assets.{ts,tsx}',
-    '!jest.config.js',
-    '!**/mock/**/*',
+    "**/*.{js,ts,jsx,tsx}",
+    "!**/index.{js,ts,jsx,tsx}",
+    "!**/*.steps.*",
+    "!**/*.d.ts",
+    "!**/*types.ts",
+    "!**/*.assets.{ts,tsx}",
+    "!jest.config.js",
+    "!**/mock/**/*",
   ],
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.test.json",
+    },
+  },
 };
 
 module.exports = merge({}, commonConfig, config);
