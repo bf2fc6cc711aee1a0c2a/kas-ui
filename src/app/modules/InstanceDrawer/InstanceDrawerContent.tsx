8004d07949b03543834ae74afba4e9fe7fae132b
suyash-naithani <suyash.naithani@gmail.com>
2022-05-05 16:45:53 +0530

    Add release flag

--- src/app/modules/InstanceDrawer/InstanceDrawerContent.tsx
+++ src/app/modules/InstanceDrawer/InstanceDrawerContent.tsx
@@ -8,6 +8,7 @@ import { ConnectionTabProps } from "@app/modules/InstanceDrawer/ConnectionTab";
 import { useInstanceDrawer } from "@app/modules/InstanceDrawer/contexts/InstanceDrawerContext";
 import { InstanceDrawerTab } from "@app/modules/InstanceDrawer/tabs";
 import { KafkaDetailsTab } from "@rhoas/app-services-ui-components";
+import DetailsTab from "./DetailsTab";
 
 export const ResourcesTab = lazy(() => import("./ConnectionTab"));
 
@@ -57,7 +58,9 @@ export const InstanceDrawerContent: FunctionComponent<
   const isKafkaPending =
     instanceDrawerInstance?.status === InstanceStatus.ACCEPTED ||
     instanceDrawerInstance?.status === InstanceStatus.PREPARING;
-
+  const testRelease = new URLSearchParams(
+    new URL(document.location.toString()).search
+  ).get("testRelease");
   return (
     <Suspense fallback={<MASLoading />}>
       <Tabs
@@ -68,32 +71,36 @@ export const InstanceDrawerContent: FunctionComponent<
           eventKey={InstanceDrawerTab.DETAILS.toString()}
           title={<TabTitleText>{t("details")}</TabTitleText>}
         >
-          <KafkaDetailsTab
-            id={id}
-            owner={owner}
-            createdAt={new Date(created_at)}
-            updatedAt={new Date(updated_at)}
-            expiryDate={addHours(new Date(created_at), 48)}
-            size={size_id}
-            ingress={ingress_throughput_per_sec?.slice(
-              0,
-              ingress_throughput_per_sec?.length - 2
-            )}
-            egress={egress_throughput_per_sec?.slice(
-              0,
-              egress_throughput_per_sec?.length - 2
-            )}
-            storage={kafka_storage_size?.slice(
-              0,
-              kafka_storage_size?.length - 2
-            )}
-            maxPartitions={max_partitions}
-            connections={total_max_connections}
-            connectionRate={max_connection_attempts_per_sec}
-            messageSize={1}
-            region={t(region)}
-            instanceType={instance_type === "standard" ? "standard" : "eval"}
-          />
+          {testRelease ? (
+            <KafkaDetailsTab
+              id={id}
+              owner={owner}
+              createdAt={new Date(created_at)}
+              updatedAt={new Date(updated_at)}
+              expiryDate={addHours(new Date(created_at), 48)}
+              size={size_id}
+              ingress={ingress_throughput_per_sec?.slice(
+                0,
+                ingress_throughput_per_sec?.length - 2
+              )}
+              egress={egress_throughput_per_sec?.slice(
+                0,
+                egress_throughput_per_sec?.length - 2
+              )}
+              storage={kafka_storage_size?.slice(
+                0,
+                kafka_storage_size?.length - 2
+              )}
+              maxPartitions={max_partitions}
+              connections={total_max_connections}
+              connectionRate={max_connection_attempts_per_sec}
+              messageSize={1}
+              region={t(region)}
+              instanceType={instance_type === "standard" ? "standard" : "eval"}
+            />
+          ) : (
+            <DetailsTab />
+          )}
         </Tab>
         <Tab
           eventKey={InstanceDrawerTab.CONNECTION.toString()}
