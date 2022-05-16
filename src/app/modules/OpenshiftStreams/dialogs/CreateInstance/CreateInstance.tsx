fbd58b18a5f2393e05f97611a178781c44e0e7cf
Ajay Pratap <apratap@redhat.com>
2022-05-16 14:49:21 +0530

    merge with base branch

--- src/app/modules/OpenshiftStreams/dialogs/CreateInstance/CreateInstance.tsx
+++ src/app/modules/OpenshiftStreams/dialogs/CreateInstance/CreateInstance.tsx
@@ -1,57 +1,6 @@
-import {
-  CreateKafkaInstance,
-  CreateKafkaInitializationData,
-  OnCreateKafka,
-} from "@rhoas/app-services-ui-components";
-import {
-  BaseModalProps,
-  CreateInstanceProps,
-} from "@rhoas/app-services-ui-shared";
-import { QuickStartContext } from "@patternfly/quickstarts";
-import { FunctionComponent, useCallback, useContext } from "react";
-import { getModalAppendTo } from "@app/utils";
-import { useAvailableProvidersAndDefault, useCreateInstance } from "./api";
+// import { CreateInstance as CreateInstanceLegacy } from "./CreateInstance";
+import { CreateInstanceWithSizes } from "./CreateInstanceWithSizes";
 
-const CreateInstance: FunctionComponent<
-  CreateInstanceProps & BaseModalProps
-> = ({ hideModal, onCreate }) => {
-  const fetchAvailableProvidersAndDefault = useAvailableProvidersAndDefault();
-  const createInstance = useCreateInstance();
-  const qsContext = useContext(QuickStartContext);
+const CreateInstance = CreateInstanceWithSizes;
 
-  const onClickQuickStart = useCallback(() => {
-    qsContext.setActiveQuickStart &&
-      qsContext.setActiveQuickStart("getting-started");
-  }, [qsContext]);
-
-  const handleCreate = useCallback<OnCreateKafka>(
-    function (data, onSuccess, onError) {
-      const handleOnSuccess = () => {
-        onSuccess();
-        onCreate && onCreate();
-        hideModal();
-      };
-      createInstance(data, handleOnSuccess, onError);
-    },
-    [hideModal, onCreate, createInstance]
-  );
-
-  const getAvailableProvidersAndDefaults =
-    useCallback(async (): Promise<CreateKafkaInitializationData> => {
-      return fetchAvailableProvidersAndDefault();
-    }, [fetchAvailableProvidersAndDefault]);
-
-  return (
-    <CreateKafkaInstance
-      isModalOpen={true}
-      onClickQuickStart={onClickQuickStart}
-      onCancel={hideModal}
-      getAvailableProvidersAndDefaults={getAvailableProvidersAndDefaults}
-      onCreate={handleCreate}
-      appendTo={getModalAppendTo}
-    />
-  );
-};
-
-export { CreateInstance };
 export default CreateInstance;
---
+++ src/app/modules/OpenshiftStreams/dialogs/CreateInstance/CreateInstanceWithSizes/hooks/useGetAvailableSizes.ts
@@ -0,0 +1,85 @@
+import { InstanceType } from "@app/utils";
+import { CreateKafkaInstanceWithSizesTypes } from "@rhoas/app-services-ui-components";
+import { Size } from "@rhoas/app-services-ui-components/types/src/Kafka/CreateKafkaInstanceWithSizes/types";
+import {
+  QuotaType,
+  useAuth,
+  useConfig,
+  useQuota,
+} from "@rhoas/app-services-ui-shared";
+import { Configuration, DefaultApi } from "@rhoas/kafka-management-sdk";
+
+/**
+ * Return list of the instance types available for the current user
+ *
+ * @returns {Promise<InstanceType[]>}
+ */
+export function useGetAvailableSizes() {
+  const { getQuota } = useQuota();
+  const { kas } = useAuth();
+  const {
+    kas: { apiBasePath: basePath },
+  } = useConfig();
+
+  return async (
+    provider: string,
+    region: string
+  ): Promise<CreateKafkaInstanceWithSizesTypes.GetSizesData> => {
+    const api = new DefaultApi(
+      new Configuration({
+        accessToken: kas.getToken(),
+        basePath,
+      })
+    );
+
+    const quota = await getQuota();
+    const kasQuota = quota?.data?.get(QuotaType?.kas);
+    const instanceType = kasQuota
+      ? InstanceType.standard
+      : InstanceType.developer;
+
+    const sizes = await api.getInstanceTypesByCloudProviderAndRegion(
+      provider,
+      region
+    );
+    if (sizes?.data?.instance_types) {
+      let instanceTypesSizes = sizes?.data?.instance_types.find(
+        (i) => i.id === instanceType
+      )?.sizes;
+      if (instanceTypesSizes) {
+        if (kasQuota) {
+          instanceTypesSizes = instanceTypesSizes.filter(
+            (s) => (s.quota_consumed || 0) <= kasQuota.remaining
+          );
+        }
+        // TODO filter sizes that do not fit to the region
+        const componentSizes = instanceTypesSizes.map((s) => {
+          return {
+            id: s.id,
+            streamingUnits: s.quota_consumed,
+            ingress: (s.ingress_throughput_per_sec?.bytes || 0) / 1048576,
+            egress: (s.egress_throughput_per_sec?.bytes || 0) / 1048576,
+            storage: Math.round(
+              (s.max_data_retention_size?.bytes || 0) / 1073741824
+            ),
+            connections: s.total_max_connections,
+            connectionRate: s.max_connection_attempts_per_sec,
+            maxPartitions: s.max_partitions,
+            // TODO https://issues.redhat.com/browse/MGDSTRM-8385
+            messageSize: 1,
+          } as Size;
+        });
+        return { sizes: componentSizes };
+      } else {
+        // TODO This case should never happen
+        console.error(
+          "Cannot match instance type to backend response.",
+          instanceType
+        );
+        return { sizes: [] };
+      }
+    }
+
+    return { sizes: [] };
+  };
+}
